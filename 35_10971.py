497267291f6e44c31a6b5edcfa49c230a197a9be
DongjinS <sdjsdj3@gmail.com>
2021-11-10 14:43:47 +0900

    permutation 함수 사용하면서 문제에서 사실상 중복되는 순열을 지움 - python3로 백준 통과

--- 35_10971.py
+++ 35_10971.py
@@ -5,15 +5,24 @@ import sys
 #조합마다 비용 계산
 def CalcTravelCost(path: list, city_map: list):
     cost = 0
-    for i in range(n - 1):
+    # 시작점
+    if city_map[0][path[0]] != 0: 
+        #print(f'city_map[0][path[0]]: {city_map[0][path[0]]}')
+        cost += city_map[0][path[0]]
+    else:
+        return 0 
+    #중간 path 
+    for i in range(n - 2):
         if city_map[path[i]][path[i+1]] != 0:
+            #print(f'city_map[path{i}][path[{i+1}]]: {city_map[path[i]][path[i+1]]}')
             cost += city_map[path[i]][path[i+1]]
         else:
             return 0
     
-    #돌아가는 비용
-    if city_map[path[n-1]][path[0]] != 0:
-        cost += city_map[path[n-1]][path[0]]
+    #마지막 path
+    if city_map[path[n-2]][0] != 0:
+        #print(f'city_map[{n-2}][0]: {city_map[n-2][0]}')
+        cost += city_map[path[n-2]][0]
     else:
         return 0
     return cost
@@ -26,29 +35,18 @@ for i in range(n):
     city_map.append(tmp)
 # city_map = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-#조합 만들기
-#permutation 쓰면 tuple 형태로
-# permu_city = list(permutations(range(n)))
 
-## 조합 전체 만들어서 저장하면 메모리 초과!!
-
-# #배열 없이 최솟값 찾기
-# for i in city_map:
-#     minimum += sum(i)
-# print(minimum)
-
-# minimum =  1000000 * 10
-# for i in permutations(range(n)):
-#     cost = CalcTravelCost(i,city_map)
-#     if cost != 0:
-#         if minimum > cost:
-#             minimum = cost
-# print(minimum)
+#시작점을 0으로 고정하고 n-1개 를 뽑으면
+## n개를 뽑을 때 (0123 1230 3012 2103) - 세개 다 같은 것 or (1032 0321 3210 2103) 같은 거 없앨 수 있다? 
+# for i in permutations(range(1,n)):
+#     print(i)
 
 #배열 만들어서 최솟값 찾기
 costs = []
-for i in permutations(range(n)):
+for i in permutations(range(1,n)):
+    #print(i)
     cost = CalcTravelCost(i,city_map)
+    #print(cost)
     if cost != 0:
         costs.append(cost)
 # print(costs, len(costs))
